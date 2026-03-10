c0672fc0d24a8c20bf3903ca1f1d2694e66eb8a9
Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>
2026-03-10 12:04:33 +0200

    extension: Change extension name
    analog-devices -> AnalogDevices so the vscode settings work
    
    Signed-off-by: Andrei-Fabian-Pop <andrei.fabian.pop@gmail.com>

--- packages/extension/src/constants.ts
+++ packages/extension/src/constants.ts
@@ -1,2 +1,2 @@
-export const EXTENSION_ID = 'analog-devices.analog-attach';
+export const EXTENSION_ID = 'AnalogDevices.analog-attach';
 export const DTSO_BASE_MAP_KEY = 'analog-attach.dtsoBaseMap';
