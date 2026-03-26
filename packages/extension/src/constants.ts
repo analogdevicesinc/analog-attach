0cee3fb9c08e69de27634c8134bfd13776726070
Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>
2026-03-26 15:25:25 +0200

    extension: Add macro for reusing extension setting prefix
    Signed-off-by: Andrei-Fabian-Pop <Andreifabian.Pop@analog.com>

--- packages/extension/src/constants.ts
+++ packages/extension/src/constants.ts
@@ -1,2 +1,3 @@
 export const EXTENSION_ID = 'AnalogDevices.analog-attach';
-export const DTSO_BASE_MAP_KEY = 'analog-attach.dtsoBaseMap';
+export const EXTENSION_SETTING_PREFIX = 'analog-attach';
+export const DTSO_BASE_MAP_KEY = `${EXTENSION_SETTING_PREFIX}.dtsoBaseMap`;
