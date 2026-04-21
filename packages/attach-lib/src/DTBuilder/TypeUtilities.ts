0544235fe9a3e28e0fbeeae4c1eecb5443cf4680
Dragos Petri <Dragos.Petri@analog.com>
2026-04-21 17:55:49 +0300

    attach-lib: DTBuilder split into interface/implementation, types and type utilities
    Signed-off-by: Dragos Petri <Dragos.Petri@analog.com>

---
+++ packages/attach-lib/src/DTBuilder/TypeUtilities.ts
@@ -0,0 +1,70 @@
+export type Labeled<T> = {
+    payload: T,
+    labels: string[]
+}
+
+export function is_labeled<T>(object: any): object is Labeled<T> {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    if (!("payload" in object)) {
+        return false;
+    }
+
+    if (object.payload === undefined) {
+        return false;
+    }
+
+    if (!("labels" in object)) {
+        return false;
+    }
+
+    if (!Array.isArray(object.labels)) {
+        return false;
+    }
+
+    if (Object.entries(object).length > 2) {
+        return false;
+    }
+
+    const narrowed = object as Labeled<T>;
+
+    if (!narrowed.labels.every((entry) => typeof entry === 'string')) {
+        return false;
+    }
+
+    return true;
+}
+
+export function make_labeled<T>(object: T, labels?: string[]): Labeled<T> {
+
+    type Labeled_T = Labeled<T>;
+    const labeled_object: Labeled_T = {
+        payload: object,
+        labels: labels ?? []
+    };
+
+    return labeled_object;
+}
+
+export type MakeArray<T> = T[];
+
+export function is_array<T>(object: any): object is MakeArray<T> {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    if (!Array.isArray(object)) {
+        return false;
+    }
+
+    return true;
+}
+
+export function make_array<T>(object: T): MakeArray<T> {
+    type Array_T = MakeArray<T>;
+    const object_array: Array_T = [object];
+
+    return object_array;
+}
---
+++ packages/attach-lib/src/DTBuilder/Types.ts
@@ -0,0 +1,249 @@
+import { CellArrayElement, DtsCellArray } from "../dts";
+import { Labeled, MakeArray, is_array, is_labeled, make_array, make_labeled } from "./TypeUtilities";
+
+type DTString = string;
+type DTString_Array = MakeArray<DTString>;
+export type LabeledDTString = Labeled<DTString>;
+type LabeledDTString_Array = MakeArray<LabeledDTString>;
+export type DTStringInput = (DTString | DTString_Array | LabeledDTString | LabeledDTString_Array);
+
+function is_dt_string(object: any): object is DTString {
+    if (object === null || typeof object !== 'string') {
+        return false;
+    }
+
+    return true;
+}
+
+function is_dts_string_array(object: any): object is DTString_Array {
+    return is_array(object) && object.every((entry) => is_dt_string(entry));
+}
+
+function is_labeled_dt_string(object: any): object is LabeledDTString {
+    return is_labeled(object) && is_dt_string(object.payload);
+}
+
+function is_labeled_dt_string_array(object: any): object is LabeledDTString_Array {
+    return is_array(object) && object.every((entry) => is_labeled_dt_string(entry));
+}
+
+function is_dt_string_input(object: any): object is DTStringInput {
+    return is_dt_string(object) ||
+        is_dts_string_array(object) ||
+        is_labeled_dt_string(object) ||
+        is_labeled_dt_string_array(object);
+}
+
+export function upcast_to_LabeledDTStringArray(input: DTStringInput): LabeledDTString_Array {
+    if (is_dt_string(input)) {
+        return make_array(make_labeled(input));
+    }
+
+    if (is_dts_string_array(input)) {
+        return input.map((entry) => make_labeled(entry));
+    }
+
+    if (is_labeled_dt_string(input)) {
+        return make_array(input);
+    }
+
+    return input;
+}
+
+export type DTSReferenceInput = DTString | LabeledDTString;
+
+function is_dts_reference_input(object: any): object is DTSReferenceInput {
+    return is_dt_string(object) || is_labeled_dt_string(object);
+}
+
+export function upcast_to_LabeledDTString(input: DTSReferenceInput): LabeledDTString {
+
+    if (is_dt_string(input)) {
+        return make_labeled(input);
+    }
+
+    return input;
+}
+
+const CELL_VALUE_TAGS = ["number", "u64", "label", "path", "expression"] as const;
+type CellValueTag = typeof CELL_VALUE_TAGS[number];
+
+type TagWith<T extends string | bigint, TAG extends CellValueTag> = {
+    _tag: TAG,
+    value: T
+}
+
+function is_tagged(object: any): object is TagWith<string | bigint, CellValueTag> {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    if (!("_tag" in object)) {
+        return false;
+    }
+
+    if (!("value" in object)) {
+        return false;
+    }
+
+    if (Object.entries(object).length > 2) {
+        return false;
+    }
+
+    const narrowed = object as TagWith<string | bigint, CellValueTag>;
+
+    if (!CELL_VALUE_TAGS.includes(narrowed._tag)) {
+        return false;
+    }
+
+    if (!['string', 'bigint'].includes(typeof narrowed.value)) {
+        return false;
+    }
+
+    return true;
+}
+
+export type CellValue =
+    TagWith<bigint, "number"> |
+    TagWith<bigint, "u64"> |
+    TagWith<string, "label"> |
+    TagWith<string, "path"> |
+    TagWith<string, "expression">
+
+type TaggedCellValue_Array = MakeArray<CellValue>;
+type TaggedCellValue_LabeledArray = Labeled<TaggedCellValue_Array>;
+
+export type LabeledTaggedCellValue = Labeled<CellValue>;
+type LabeledTaggedCellValue_Array = MakeArray<LabeledTaggedCellValue>;
+export type LabeledTaggedCellValue_LabeledArray = Labeled<LabeledTaggedCellValue_Array>;
+
+export type CellEntry = CellValue | LabeledTaggedCellValue;
+export type CellArray = TaggedCellValue_Array | TaggedCellValue_LabeledArray | LabeledTaggedCellValue_Array | LabeledTaggedCellValue_LabeledArray;
+export type CellMatrix = MakeArray<CellArray>;
+export type DTCellArrayInput = CellEntry | CellArray | CellMatrix;
+
+function is_tagged_cell_value_array(object: any): object is TaggedCellValue_Array {
+    return is_array(object) && object.every((entry) => is_tagged(entry) === true);
+}
+
+function is_tagged_cell_value_labeled_array(object: any): object is TaggedCellValue_LabeledArray {
+    return is_labeled(object) && is_tagged_cell_value_array(object.payload);
+}
+
+function is_labeled_tagged_cell_value(object: any): object is LabeledTaggedCellValue {
+    return is_labeled(object) && is_tagged(object.payload);
+}
+
+function is_labeled_tagged_cell_value_array(object: any): object is LabeledTaggedCellValue_Array {
+    return is_array(object) && object.every((entry) => is_labeled_tagged_cell_value(entry) === true);
+}
+
+export function is_labeled_tagged_cell_value_labeled_array(object: any): object is LabeledTaggedCellValue_LabeledArray {
+    return is_labeled(object) && is_labeled_tagged_cell_value_array(object.payload);
+}
+
+export function is_cell_entry(object: any): object is CellEntry {
+    return is_tagged(object) || is_labeled_tagged_cell_value(object);
+}
+
+export function is_cell_array(object: any): object is CellArray {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    return is_tagged_cell_value_array(object) ||
+        is_tagged_cell_value_labeled_array(object) ||
+        is_labeled_tagged_cell_value_array(object) ||
+        is_labeled_tagged_cell_value_labeled_array(object);
+}
+
+export function is_cell_matrix(object: any): object is CellMatrix {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    if (!is_array(object)) {
+        return false;
+    }
+
+    if (!object.every((entry) => is_cell_array(entry) === true)) {
+        return false;
+    }
+
+    return true;
+}
+
+export function is_cell_entry_or_cell_array(object: any): object is (CellEntry | CellArray) {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    return is_cell_entry(object) || is_cell_array(object);
+}
+
+export function is_cell_array_or_cell_matrix(object: any): object is (CellArray | CellMatrix) {
+    if (object === null || typeof object !== 'object') {
+        return false;
+    }
+
+    return is_cell_array(object) || is_cell_matrix(object);
+}
+
+export function upcast_to_LabeledTaggedCellValue_LabeledArray(input: CellEntry | CellArray): LabeledTaggedCellValue_LabeledArray {
+
+    if (is_tagged_cell_value_array(input)) {
+        return make_labeled(input.map((entry) => make_labeled(entry)));
+    }
+
+    if (is_tagged_cell_value_labeled_array(input)) {
+        return make_labeled(input.payload.map((entry) => make_labeled(entry)));
+    }
+
+    if (is_tagged(input)) {
+        return make_labeled(make_array(make_labeled(input)));
+    }
+
+    if (is_labeled_tagged_cell_value_array(input)) {
+        return make_labeled(input);
+    }
+
+    if (is_labeled_tagged_cell_value(input)) {
+        return make_labeled(make_array(input));
+    }
+
+    return input;
+}
+
+export function make_cell_array(values: LabeledTaggedCellValue_LabeledArray): DtsCellArray {
+
+    // eslint-disable-next-line unicorn/consistent-function-scoping
+    const tagged_to_element = (entry: LabeledTaggedCellValue): CellArrayElement => {
+        switch (entry.payload._tag) {
+            case "number": {
+                return { item: { kind: "number", value: entry.payload.value, labels: entry.labels } };
+            }
+            case "u64": {
+                return { item: { kind: "u64", value: entry.payload.value, labels: entry.labels } };
+            }
+            case "label": {
+                return { item: { kind: "ref", labels: entry.labels, ref: { kind: "label", name: entry.payload.value } } };
+            }
+            case "path": {
+                return { item: { kind: "ref", labels: entry.labels, ref: { kind: "path", path: entry.payload.value } } };
+            }
+            case "expression": {
+                return { item: { kind: "expression", labels: entry.labels, value: entry.payload.value } };
+            }
+            default: {
+                const _x: never = entry.payload;
+                throw new Error("Exhaustive check failed!");
+            }
+        }
+    };
+
+    return {
+        kind: "array",
+        labels: values.labels,
+        elements: values.payload.map((v) => tagged_to_element(v)),
+    };
+}
