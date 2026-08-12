eceb2879c30f8c407aac3e98450b727ebad280fd
Dragos Petri <Dragos.Petri@analog.com>
2026-08-12 13:43:15 +0300

    attach-lib: split DTBuilder.ts in PropertyBuilder.ts and NodeBuilder.ts
    Signed-off-by: Dragos Petri <Dragos.Petri@analog.com>

---
+++ packages/attach-lib/src/DTBuilder/NodeBuilder.ts
@@ -0,0 +1,76 @@
+import { DTNode, DTProperty } from "../dts";
+import { AddCallOnce } from "./TypeUtilities";
+
+interface INodeNameBuilder {
+    with_name: (name: string) => INodeBuilder;
+}
+
+export interface INodeBuilderBase {
+    build: () => DTNode;
+}
+
+interface INodeBuilderCallOnce {
+    with_label: (label: string | string[]) => void;
+    with_unit_address: (unit_address: string | undefined) => void;
+    with_properties: (properties: DTProperty | DTProperty[] | undefined) => void;
+    with_children: (children: INodeBuilderBase | INodeBuilderBase[] | undefined) => void;
+}
+
+type INodeBuilder = AddCallOnce<INodeBuilderBase, INodeBuilderCallOnce>;
+
+export class NodeBuilder implements INodeNameBuilder, INodeBuilder {
+
+    private node: DTNode = {
+        labels: [],
+        name: "",
+        unit_addr: undefined,
+        properties: [],
+        children: [],
+    };
+
+    private constructor() { }
+
+    static new(): INodeNameBuilder {
+        return new NodeBuilder;
+    }
+
+    build(): DTNode {
+        return this.node;
+    }
+
+    with_label(label: string | string[]): INodeBuilder {
+        this.node.labels = Array.isArray(label) ? label : [label];
+        return this;
+    }
+
+    with_name(name: string): INodeBuilder {
+        this.node.name = name;
+        return this;
+    }
+
+    with_unit_address(unit_address: string | undefined): INodeBuilder {
+        if (unit_address !== undefined) {
+            this.node.unit_addr = unit_address;
+        }
+        return this;
+    }
+
+    with_properties(properties: DTProperty | DTProperty[] | undefined): INodeBuilder {
+        if (properties === undefined) {
+            return this;
+        }
+
+        this.node.properties = Array.isArray(properties) ? properties : [properties];
+        return this;
+    }
+
+    with_children(children: INodeBuilderBase | INodeBuilderBase[] | undefined): INodeBuilder {
+        if (children === undefined) {
+            return this;
+        }
+
+        const childArray = Array.isArray(children) ? children : [children];
+        this.node.children = childArray.map(child => child.build());
+        return this;
+    }
+}
--- packages/attach-lib/src/DTBuilder/PropertyBuilder.ts
+++ packages/attach-lib/src/DTBuilder/PropertyBuilder.ts
@@ -1,4 +1,4 @@
-import { Bits, DTCellArray, DTNode, DTProperty } from "../dts";
+import { Bits, DTCellArray, DTProperty } from "../dts";
 import { print_property } from "../dts/printer";
 import {
     CellArray,
@@ -192,80 +192,6 @@ export class PropertyBuilder implements
     static tag_label(value: string) { return { _tag: "label" as const, value: value }; };
     static tag_path(value: string) { return { _tag: "path" as const, value: value }; };
     static tag_expression(value: string) { return { _tag: "expression" as const, value: value }; };
-}
-
-interface INodeNameBuilder {
-    with_name: (name: string) => INodeBuilder;
-}
-
-export interface INodeBuilderBase {
-    build: () => DTNode;
-}
-
-interface INodeBuilderCallOnce {
-    with_label: (label: string | string[]) => void;
-    with_unit_address: (unit_address: string | undefined) => void;
-    with_properties: (properties: DTProperty | DTProperty[] | undefined) => void;
-    with_children: (children: INodeBuilderBase | INodeBuilderBase[] | undefined) => void;
-}
-
-type INodeBuilder = AddCallOnce<INodeBuilderBase, INodeBuilderCallOnce>;
-
-export class NodeBuilder implements INodeNameBuilder, INodeBuilder {
-
-    private node: DTNode = {
-        labels: [],
-        name: "",
-        unit_addr: undefined,
-        properties: [],
-        children: [],
-    };
-
-    private constructor() { }
-
-    static new(): INodeNameBuilder {
-        return new NodeBuilder;
-    }
-
-    build(): DTNode {
-        return this.node;
-    }
-
-    with_label(label: string | string[]): INodeBuilder {
-        this.node.labels = Array.isArray(label) ? label : [label];
-        return this;
-    }
-
-    with_name(name: string): INodeBuilder {
-        this.node.name = name;
-        return this;
-    }
-
-    with_unit_address(unit_address: string | undefined): INodeBuilder {
-        if (unit_address !== undefined) {
-            this.node.unit_addr = unit_address;
-        }
-        return this;
-    }
-
-    with_properties(properties: DTProperty | DTProperty[] | undefined): INodeBuilder {
-        if (properties === undefined) {
-            return this;
-        }
-
-        this.node.properties = Array.isArray(properties) ? properties : [properties];
-        return this;
-    }
-
-    with_children(children: INodeBuilderBase | INodeBuilderBase[] | undefined): INodeBuilder {
-        if (children === undefined) {
-            return this;
-        }
-
-        const childArray = Array.isArray(children) ? children : [children];
-        this.node.children = childArray.map(child => child.build());
-        return this;
-    }
 }
 
 if (import.meta.vitest !== undefined) {
